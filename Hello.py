c9b4d57c61bb6b3b7851b7ab032672f13a80bfdf
Hossam-Eldin Mohammed <82487239+H0SS4M@users.noreply.github.com>
2024-01-19 03:13:41 +0200

    fixing critical bug

--- Hello.py
+++ Hello.py
@@ -4,4 +4,4 @@ import random
 l = ['TriageAtDawn', 'SilverDragon', 'SOMA', 'LDawly', '3m 3ly']
 
 if st.button("Reset", type="primary"):
-  st.markdown(f"{l[random.randint(0, len(l))]} is {random.randint(0, 100)}% NOOB")
+  st.markdown(f"{l[random.randint(0, len(l)-1)]} is {random.randint(0, 100)}% NOOB")
