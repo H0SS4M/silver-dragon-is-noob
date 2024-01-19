577b0eaa4a287a276618b55bdc106c1ae6cff25f
Hossam-Eldin Mohammed <82487239+H0SS4M@users.noreply.github.com>
2024-01-19 03:37:46 +0200

    s

--- Hello.py
+++ Hello.py
@@ -4,8 +4,11 @@ import random
 l = ['TriageAtDawn', 'SilverDragon', 'SOMA', 'LDawly', '3m 3ly']
 
 text = st.text_input('Enter your name')
-if ((text or text != '') and text.lower() not in map(str.lower, l)):
-  l.append(text)
+if ((text or text != '')):
+  if (text.lower() not in map(str.lower, l)):
+    st.write("name entered successfully")
+  else:
+    st.write("name found before")
 
-if st.button("I feel lucky", type="primary"):
+if st.button("I feel lucky"):
   st.markdown(f"{l[random.randint(0, len(l)-1)]} is {random.randint(0, 100)}% NOOB")
