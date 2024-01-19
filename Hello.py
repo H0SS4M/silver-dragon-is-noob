8360973e3feb8c599c4800923101f0a6d6a0d7ac
Hossam-Eldin Mohammed <82487239+H0SS4M@users.noreply.github.com>
2024-01-19 03:16:13 +0200

    fsd

--- Hello.py
+++ Hello.py
@@ -3,5 +3,5 @@ import random
 
 l = ['TriageAtDawn', 'SilverDragon', 'SOMA', 'LDawly', '3m 3ly']
 
-if st.button("Reset", type="primary"):
+if st.button("I feel lucky", type="primary"):
   st.markdown(f"{l[random.randint(0, len(l)-1)]} is {random.randint(0, 100)}% NOOB")
