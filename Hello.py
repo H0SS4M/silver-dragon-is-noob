8e53b296c44645f25831d74a8815fb3ee5e5e3cf
Hossam-Eldin Mohammed <82487239+H0SS4M@users.noreply.github.com>
2024-01-19 03:28:06 +0200

    fwef

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ import random
 l = ['TriageAtDawn', 'SilverDragon', 'SOMA', 'LDawly', '3m 3ly']
 
 text = st.text_input('Enter your name')
-if ((text or text != '') and text not in l):
+if ((text or text != '') and text.lower() not in map(str.lower, l)):
   l.append(text)
 
 if st.button("I feel lucky", type="primary"):
